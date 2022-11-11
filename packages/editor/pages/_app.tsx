d1d3597a666e173d5e307708b818a6c84013d160
Laishuxin <15622971239@163.com>
2022-11-11 21:37:13 +0800

    init: app

---
+++ packages/editor/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@popup/theme/src/index.scss'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
